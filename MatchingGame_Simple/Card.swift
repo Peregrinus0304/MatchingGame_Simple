5b06929dff9e1bc1512f640f13f90cee69aae0b6
Perefrinus <motruk007@gmail.com>
2020-12-28 17:33:14 +0200

    add Card class

---
+++ MatchingGame_Simple/Card.swift
@@ -0,0 +1,16 @@
+//
+//  Card.swift
+//  MatchingGame_Simple
+//
+//  Created by Ozzy on 28.12.2020.
+//  Copyright © 2020 Taras Motruk. All rights reserved.
+//
+
+import Foundation
+
+class Card {
+    var imageName = ""
+    var isFlipped = false
+    var isMatched = false
+    
+}
