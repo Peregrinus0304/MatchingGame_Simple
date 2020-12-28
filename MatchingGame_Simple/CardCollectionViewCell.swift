4a6f3eda0353e2da1e7422d61610616bcc652680
Perefrinus <motruk007@gmail.com>
2020-12-28 19:40:37 +0200

    create setUpCards func in CollectionView cell` class

--- MatchingGame_Simple/CardCollectionViewCell.swift
+++ MatchingGame_Simple/CardCollectionViewCell.swift
@@ -10,4 +10,38 @@ import UIKit
 
 class CardCollectionViewCell: UICollectionViewCell {
     
+    
+    @IBOutlet weak var frontImageView: UIImageView!
+    @IBOutlet weak var backImageView: UIImageView!
+    
+      var card:Card?
+    
+    func setCard(_ card:Card) {
+        
+        self.card = card
+        
+        if card.isMatched == true{
+            backImageView.alpha = 0
+            frontImageView.alpha = 0
+            
+             return
+        }
+        else {
+            backImageView.alpha = 1
+            frontImageView.alpha = 1
+        }
+        
+        frontImageView.image = UIImage(named: card.imageName)
+        if card.isFlipped == true {
+         
+            UIView.transition(from: backImageView, to: frontImageView, duration: 0, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: nil)
+        }
+        else {
+            UIView.transition(from: frontImageView, to: backImageView, duration: 0, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: nil)
+            
+        }
+    }
+    
+    #warning("add funks to flip, flip back and remove cards")
+    
 }
