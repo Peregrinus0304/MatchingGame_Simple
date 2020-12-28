e7f180ad59f99f4c52f49600b49423ce0a7febda
Perefrinus <motruk007@gmail.com>
2020-12-28 19:10:40 +0200

    add timer & CollectionViewCell

---
+++ MatchingGame_Simple/CardCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CardCollectionViewCell.swift
+//  MatchingGame_Simple
+//
+//  Created by Ozzy on 28.12.2020.
+//  Copyright © 2020 Taras Motruk. All rights reserved.
+//
+
+import UIKit
+
+class CardCollectionViewCell: UICollectionViewCell {
+    
+}
--- MatchingGame_Simple/ViewController.swift
+++ MatchingGame_Simple/ViewController.swift
@@ -18,27 +18,46 @@ class ViewController: UIViewController {
     //MARK: - Properties
     
     var cardArray = [Card]()
+    var model = CardModel()
+    var timer:Timer?
+    var milliseconds:Float = 60 * 1000
     
     //MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
+
+        cardArray =  model.getCards()
+        timer = Timer.scheduledTimer(timeInterval: 0.001, target: self, selector: #selector(TimerElapsed), userInfo: nil, repeats: true)
+        RunLoop.main.add(timer!, forMode:  .common)
     }
 
+    @objc func TimerElapsed () {
+         milliseconds -= 1
+         let seconds = String(format: "%.2f", milliseconds/1000)
+         timerLabel.text = "Час до анального покарання: \(seconds)"
+         if milliseconds <= 0 {
+             timer?.invalidate()
+             timerLabel.textColor = UIColor.red
+               #warning("check if game ended")
+         }
+    }
+    
 }
 
     // MARK: - Extension UICollectionViewDelegate
 
 extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+   
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        <#code#>
+       
+        return 1
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
+       let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cardCell", for: indexPath) as! CardCollectionViewCell
+        return cell
     }
     
-    
+
 }
