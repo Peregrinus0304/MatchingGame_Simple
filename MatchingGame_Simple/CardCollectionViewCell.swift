96aa5f3053c236dec585ea88ecd76690c6ffbc1d
Perefrinus <motruk007@gmail.com>
2020-12-28 20:12:03 +0200

    add cards flipping, flipping back and removing animations to CardCollectionViewCell

--- MatchingGame_Simple/CardCollectionViewCell.swift
+++ MatchingGame_Simple/CardCollectionViewCell.swift
@@ -42,6 +42,22 @@ class CardCollectionViewCell: UICollectionViewCell {
         }
     }
     
-    #warning("add funks to flip, flip back and remove cards")
+    func flip() {
+        UIView.transition(from: backImageView, to: frontImageView, duration: 0.3, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: nil)
+    }
+    
+    func flipBack() {
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.5) {
+            UIView.transition(from: self.frontImageView, to: self.backImageView, duration: 0.3, options: [.transitionFlipFromRight, .showHideTransitionViews], completion: nil)
+        }
+        
+    }
+    func remove() {
+        backImageView.alpha = 0
+
+        UIView.animate(withDuration: 0.3, delay: 0.5, options: .curveEaseOut, animations: {
+            self.frontImageView.alpha = 0
+        }, completion: nil)
+    }
     
 }
